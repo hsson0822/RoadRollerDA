e69617be7ed7a38ccbe87eac0232f9d86d536d39
hsson0822 <50266823+hsson0822@users.noreply.github.com>
2025-07-22 17:11:26 +0900

    feat: 조작방식 토글 선택에 따른 변수 값 변경 추가

--- Assets/Scripts/Menu/OptionUI.cs
+++ Assets/Scripts/Menu/OptionUI.cs
@@ -4,11 +4,15 @@ using UnityEngine.UI;
 public class OptionUI : MonoBehaviour
 {
     [SerializeField] private Button returnButton;
+    [SerializeField] private Toggle[] controlToggles = new Toggle[3];
 
 
     private void OnEnable()
     {
         returnButton.onClick.AddListener(OnClickReturnButton);
+
+        for(int i = 0; i < 3; ++i)
+            controlToggles[i].onValueChanged.AddListener(delegate { SelectControlType(); });
     }
 
     private void OnDisable()
@@ -20,5 +24,15 @@ public class OptionUI : MonoBehaviour
     {
         if (GameManager.Instance.menuUI != null) GameManager.Instance.menuUI.gameObject.SetActive(true);
         gameObject.SetActive(false);
+    }
+
+
+    private void SelectControlType()
+    {
+        for (int i = 0; i < 3; ++i)
+            if (controlToggles[i].isOn)
+                GameManager.Instance.controlType = (ControlType)i;
+
+        Debug.Log(GameManager.Instance.controlType);
     }
 }
